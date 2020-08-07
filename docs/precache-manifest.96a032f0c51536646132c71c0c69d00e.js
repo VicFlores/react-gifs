8f4ea3beb493edb356ad11e470375621aa1429ef
Vic Flores <vicsito2014@gmail.com>
2020-08-07 09:43:48 -0600

    /docs created

---
+++ docs/precache-manifest.96a032f0c51536646132c71c0c69d00e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1940071145e7e65e07d0b694758c183",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2505c5ae8a0d4d3b97b4",
+    "url": "/static/css/main.aaa0d64a.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2505c5ae8a0d4d3b97b4",
+    "url": "/static/js/main.1824205a.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
